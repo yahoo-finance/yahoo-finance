f1f0b4a7233dd29e080c8dc4e50f6617b334380a
Lukasz Banasiak <lukasz@banasiak.me>
2014-05-02 17:55:13 +0200

    init

---
+++ yahoo_finance/__init__.py
@@ -0,0 +1,57 @@
+import yql
+
+from datetime import datetime
+import pytz
+
+__author__ = 'Lukasz Banasiak'
+__version__ = '0.5.0'
+
+
+def edt_to_utc(date, mask='%d/%m/%Y %I:%M%p'):
+    utc = pytz.utc
+    eastern = pytz.timezone('US/Eastern')
+    date_ = datetime.strptime(date, mask)
+    date_eastern = eastern.localize(date_, is_dst=None)
+    date_utc = date_eastern.astimezone(utc)
+    return date_utc
+
+
+class Currency(object):
+
+    def __init__(self, symbol):
+        self.symbol = symbol
+        self.rate = self._fetch()
+
+    @staticmethod
+    def __request(symbol):
+        response = yql.YQLQuery().execute('select * from yahoo.finance.xchange where pair in ("%s")' % symbol)
+        if 'query' in response and 'results' in response['query']:
+            return response['query']['results']
+        elif 'error' in response:
+            print 'YQL query failed with error: "%s".' % response['error']['description']
+        else:
+            print 'YQL response malformed.'
+
+    def _fetch(self):
+        data = self.__request(self.symbol)['rate']
+        data['Datetime'] = edt_to_utc('%s %s' % (data['Date'], data['Time'])).strftime('%Y-%m-%d %H:%M:%S %Z%z')
+        del data['Date'], data['Time']
+        return data
+
+    def refresh(self):
+        self.rate = self._fetch()
+
+    def get_bid(self):
+        print self.rate['Bid']
+
+    def get_ask(self):
+        print self.rate['Ask']
+
+    def get_rate(self):
+        print self.rate['Rate']
+
+
+class Share(object):
+    pass
+
+
---
+++ yahoo_finance/yql.py
@@ -0,0 +1,52 @@
+"""
+Yahoo! Python SDK
+
+ * Yahoo! Query Language
+ * Yahoo! Social API
+
+Find documentation and support on Yahoo! Developer Network: http://developer.yahoo.com
+
+Hosted on GitHub: http://github.com/yahoo/yos-social-python/tree/master
+
+@copyright: Copyrights for code authored by Yahoo! Inc. is licensed under the following terms:
+@license:   BSD Open Source License
+
+  Permission is hereby granted, free of charge, to any person obtaining a copy
+  of this software and associated documentation files (the "Software"), to deal
+  in the Software without restriction, including without limitation the rights
+  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+  copies of the Software, and to permit persons to whom the Software is
+  furnished to do so, subject to the following conditions:
+
+  The above copyright notice and this permission notice shall be included in
+  all copies or substantial portions of the Software.
+
+  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+  THE SOFTWARE.
+"""
+
+__author__   = 'Dustin Whittle <dustin@yahoo-inc.com>'
+__version__  = '0.1'
+
+import httplib, urllib, simplejson
+
+# Yahoo! YQL API
+PUBLIC_API_URL  = 'http://query.yahooapis.com/v1/public/yql'
+OAUTH_API_URL   = 'http://query.yahooapis.com/v1/yql'
+DATATABLES_URL  = 'store://datatables.org/alltableswithkeys'
+
+class YQLQuery(object):
+
+  def __init__(self):
+    self.connection = httplib.HTTPConnection('query.yahooapis.com')
+
+  def execute(self, yql, token = None):
+
+    self.connection.request('GET', PUBLIC_API_URL + '?' + urllib.urlencode({ 'q': yql, 'format': 'json', 'env': DATATABLES_URL }))
+    return simplejson.loads(self.connection.getresponse().read())
+
