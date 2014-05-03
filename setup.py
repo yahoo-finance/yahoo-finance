a161bb4dff55e8d34bcc83a30dc240561c2680e4
Lukasz Banasiak <lukasz@banasiak.me>
2014-05-03 19:08:03 +0200

    setuptools package

---
+++ setup.py
@@ -0,0 +1,103 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+import re
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+# Read the version number from a source file.
+# Why read it, and not import?
+# see https://groups.google.com/d/topic/pypa-dev/0PkjVpcxTzQ/discussion
+def find_version(*file_paths):
+    # Open in Latin-1 so that we avoid encoding errors.
+    # Use codecs.open for Python 2 compatibility
+    with codecs.open(os.path.join(here, *file_paths), 'r', 'latin1') as f:
+        version_file = f.read()
+
+    # The version line must have the form
+    # __version__ = 'ver'
+    version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]",
+                              version_file, re.M)
+    if version_match:
+        return version_match.group(1)
+    raise RuntimeError("Unable to find version string.")
+
+
+# Get the long description from the relevant file
+with codecs.open('README.rst', encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name="yahoo-finance",
+    version=find_version('yahoo_finance', '__init__.py'),
+    description="Python module to get stock data from Yahoo! Finance",
+    long_description=long_description,
+
+    # The project URL.
+    url='https://github.com/lukaszbanasiak/yahoo-finance',
+    download_url='https://github.com/lukaszbanasiak/yahoo-finance/releases',
+
+    # Author details
+    author='Lukasz Banasiak',
+    author_email='lukasz@banasiak.me',
+
+    # Choose your license
+    license='MIT',
+
+    classifiers=[
+        # How mature is this project? Common values are
+        # 3 - Alpha
+        # 4 - Beta
+        # 5 - Production/Stable
+        'Development Status :: 5 - Production/Stable',
+
+        # Indicate who your project is intended for
+        'Intended Audience :: Developers',
+        'Intended Audience :: Financial and Insurance Industry',
+        'Topic :: Office/Business :: Financial :: Investment',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Operating System :: OS Independent',
+
+        # Pick your license as you wish (should match "license" above)
+        'License :: OSI Approved :: MIT License',
+
+        # Specify the Python versions you support here. In particular, ensure
+        # that you indicate whether you support Python 2, Python 3 or both.
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+    ],
+
+    # What does your project relate to?
+    keywords='stocks market finance yahoo quotes shares currency',
+
+    # You can just specify the packages manually here if your project is
+    # simple. Or you can use find_packages.
+    packages=find_packages(exclude=["contrib", "docs", "tests*"]),
+
+    # List run-time dependencies here. These will be installed by pip when your
+    # project is installed.
+    install_requires = ['pytz', 'simplejson'],
+
+    # If there are data files included in your packages that need to be
+    # installed, specify them here. If using Python 2.6 or less, then these
+    # have to be included in MANIFEST.in as well.
+    package_data={
+        'yahoo-finance': [],
+    },
+
+    # Although 'package_data' is the preferred approach, in some case you may
+    # need to place data files outside of your packages.
+    # see http://docs.python.org/3.4/distutils/setupscript.html#installing-additional-files
+    # In this case, 'data_file' will be installed into '<sys.prefix>/my_data'
+    # data_files=[('my_data', ['data/data_file'])],
+
+    # To provide executable scripts, use entry points in preference to the
+    # "scripts" keyword. Entry points provide cross-platform support and allow
+    # pip to create the appropriate form of executable for the target platform.
+    entry_points={
+        'console_scripts': [
+            'yahoo-finance=yahoo_finance:main',
+        ],
+    },
+)
