8c0051fcac88d7cda5c0c1f5af22e0f220b73a00
Lev Lybin <lev.lybin@gmail.com>
2015-07-19 14:44:03 +0700

    removed unsupported 2.6

--- setup.py
+++ setup.py
@@ -67,12 +67,10 @@ setup(
         # Specify the Python versions you support here. In particular, ensure
         # that you indicate whether you support Python 2, Python 3 or both.
         'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
     ],
 
     # What does your project relate to?
